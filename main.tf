0eb702b9ae5cdfa39abb36d3a231705cc5ec01c4
Yoshua Wuyts <i@yoshuawuyts.com>
2016-03-01 14:13:33 +1100

    stack: no postprocess

--- main.tf
+++ main.tf
@@ -105,40 +105,10 @@ resource "aws_key_pair" "auth" {
 }
 
 resource "aws_instance" "web" {
-  # The connection block tells our provisioner how to
-  # communicate with the resource (instance)
-  connection {
-    # The default username for our AMI
-    user = "ubuntu"
+  instance_type = "t2.micro"
 
-    # The connection will use the local SSH agent for authentication.
-  }
-
-  instance_type = "m1.micro"
-
-  # Lookup the correct AMI based on the region
-  # we specified
   ami = "${lookup(var.aws_amis, var.aws_region)}"
-
-  # The name of our SSH keypair we created above.
   key_name = "${aws_key_pair.auth.id}"
-
-  # Our Security group to allow HTTP and SSH access
   vpc_security_group_ids = ["${aws_security_group.default.id}"]
-
-  # We're going to launch into the same subnet as our ELB. In a production
-  # environment it's more common to have a separate private subnet for
-  # backend instances.
   subnet_id = "${aws_subnet.default.id}"
-
-  # We run a remote provisioner on the instance after creating it.
-  # In this case, we just install nginx and start it. By default,
-  # this should be on port 80
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get -y update",
-      "sudo apt-get -y install nginx",
-      "sudo service nginx start"
-    ]
-  }
 }
